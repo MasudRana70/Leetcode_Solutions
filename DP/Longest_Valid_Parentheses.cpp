2f0f94b3b6e7830a57850395214fc9de4ae433d7
MasudRana70 <masudranaop2004@gmail.com>
2026-09-22 23:59:29 +0600

    longest valid parentheses

---
+++ DP/Longest_Valid_Parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        int ans = 0;
+
+        if(s.size() == 0) return 0;
+        st.push(-1);
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ')'){
+                st.pop();
+
+                if(st.empty()){
+                    st.push(i);
+                }
+                else ans = max(ans, i - st.top());
+            }
+            else st.push(i);
+        }
+
+        return ans;
+    }
+};
