b42bdce58a94033bae554a08bc12b16f9b9d9b56
MasudRana70 <masudranaop2004@gmail.com>
2026-09-23 01:13:45 +0600

    Decode ways

---
+++ DP/Decode_Ways.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+
+        vector<int> dp(n + 1);
+        dp[0] = 1, dp[1] = 1;
+
+        if(s[0] == '0') return 0;
+
+        for(int i = 2; i <= n; i++){
+            if((s[i-1] - '0') != 0){
+                dp[i] += dp[i-1];
+            }
+            int val = (s[i-2] - '0') * 10 + (s[i-1] - '0');
+
+            if(val >= 10 && val <= 26){
+                dp[i] += dp[i-2];
+            }
+        }
+
+        return dp[n];
+    }
+};
