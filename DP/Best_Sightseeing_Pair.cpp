99a86799935f8bedfe05aac0a3f391f741a508a0
MasudRana70 <masudranaop2004@gmail.com>
2026-09-24 02:13:18 +0600

    Best Sightseeing Pair

--- DP/Best_Sightseeing_Pair.cpp
+++ DP/Best_Sightseeing_Pair.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int mx = values[0] + values[1] - 1;
-
-        map<int, vector<pair<int, int>>> mp;
-        mp[values[0]].push_back({values[0], 0});
-        mp[values[1] + 1].push_back({values[1],  1});
+        int mx = values[0] + values[1] - 1, lmx = max(values[0] , values[1] + 1);
 
         for(int i = 2; i < values.size(); i++){
-            auto it = prev(mp.end());
-
-            mx = max(mx, it->first + values[i] - i);
-            mp[values[i] + i].push_back({values[i], i});
+            mx = max(mx, lmx + values[i] - i);
+            lmx = max(lmx, values[i] + i);
         }
 
         return mx;
